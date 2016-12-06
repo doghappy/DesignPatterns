082276839dc758c4f0ff6461505519e9a942d691
HeroWong <HeroWong@HEROWONG-WORK>
2016-12-06 11:01:32 +0800

    添加项目文件。

---
+++ 01简单工厂/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01简单工厂
+{
+    abstract class Operation
+    {
+        protected Operation(double a,double b)
+        {
+            NumberA = a;
+            NumberB = b;
+        }
+
+        public double NumberA { get; private set; }
+        public double NumberB { get; private set; }
+
+        public abstract double Result { get; }
+    }
+}
---
+++ 01简单工厂/OperationAdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01简单工厂
+{
+    class OperationAdd:Operation
+    {
+        public OperationAdd(double a, double b) : base(a, b)
+        {
+        }
+
+        public override double Result
+        {
+            get { return NumberA + NumberB; }
+        }
+    }
+}
