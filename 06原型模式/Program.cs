77b2249618d5a1db3428c948336bc3f158b80474
HeroWong <hero_wong@outlook.com>
2016-12-06 17:47:17 +0800

    原型模式

---
+++ 06原型模式/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06原型模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume a = new Resume("郭靖");
+            a.SetPersonalInfo('男', 38);
+            a.SetWorkExperience("1230-1273", "襄阳城");
+
+            Resume b = a.Clone() as Resume;
+            b.SetWorkExperience("1243-1273", "武林盟主");
+
+            Resume c = a.Clone() as Resume;
+            c.Name = "黄蓉";
+            c.SetPersonalInfo('女', 24);
+
+            a.Display();
+            b.Display();
+            c.Display();
+
+            Console.ReadKey();
+        }
+
+        /*
+         * 原型模式(Prototype)，用原型实例指定创建对象的种类，并且通过拷贝这些原型
+         * 创建新的对象
+         * 
+         * 原型模式其实就是从一个对象再创建另外一个可定制的对象，而且不需知道任何创
+         * 建细节。
+         * 
+         * 如果构造函数的执行时间很长，那么多次new效率太低了，一般是在初始化的信息不
+         * 发生变化的情况下，克隆时最好的办法，这既隐藏了对象创建细节，又对性能是大大
+         * 的提高。
+         */
+    }
+}
---
+++ 06原型模式/Resume.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06原型模式
+{
+    /// <summary>
+    /// 简历
+    /// </summary>
+    class Resume : ICloneable
+    {
+        public string Name { get; set; }
+        public char Gender { get; private set; }
+        public int Age { get; private set; }
+        public string TimeArea { get; private set; }
+        public string Company { get; private set; }
+
+        public Resume(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// 设置个人信息
+        /// </summary>
+        /// <param name="gender"></param>
+        /// <param name="age"></param>
+        public void SetPersonalInfo(char gender, int age)
+        {
+            Gender = gender;
+            Age = age;
+        }
+
+        /// <summary>
+        /// 设置个人信息
+        /// </summary>
+        /// <param name="timeArea"></param>
+        /// <param name="company"></param>
+        public void SetWorkExperience(string timeArea, string company)
+        {
+            TimeArea = timeArea;
+            Company = company;
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"{Name} {Gender} {Age}");
+            Console.WriteLine($"工作经历：{TimeArea} {Company}");
+        }
+
+        /*
+         * MemberwiseClone()方法，如果字段是值类型的，则对该字段执行逐位赋值，
+         * 如果是引用类型，则赋值引用的对象；因此，原始对象及其副本引用同一对象
+         * 这叫做“浅复制”
+         * 
+         * 如果想要“深复制”，字段、属性对应的类型也需要实现ICloneable接口
+         */
+        public object Clone() => MemberwiseClone();
+    }
+}
